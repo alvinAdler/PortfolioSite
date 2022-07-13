5fffabb4781718142d55c9853c165981edf77bbc
alvinAdler <alvinadlersampow@gmail.com>
2022-07-13 18:19:17 +0800

    Implemented the responsive version of the "about me" section and the static version of the "projects" section

---
+++ src/utilities/projectsData.js
@@ -0,0 +1,24 @@
+const projectsData = [
+    {
+        projectAbb: "STP",
+        projectTitle: "Simple Timeline Post",
+        projectDesc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed?"
+    },
+    {
+        projectAbb: "TTA",
+        projectTitle: "Game and Drill-based Touch Typing Education Application",
+        projectDesc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed?"
+    },
+    {
+        projectAbb: "APB",
+        projectTitle: "Aplikasi Pelaporan BPPTIK",
+        projectDesc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed?"
+    },
+    {
+        projectAbb: "LIM",
+        projectTitle: "Laundry Invoice Management System",
+        projectDesc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quibusdam sit earum modi laborum adipisci quae molestiae necessitatibus laboriosam sed?"
+    },
+]
+
+export default projectsData
