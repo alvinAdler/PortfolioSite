4ca1fc1608f1f39d17c3b985ee28409b3e3cd1c9
alvinAdler <alvinadlersampow@gmail.com>
2022-07-14 14:46:17 +0800

    Moved all of the tools data into a separated file

---
+++ src/utilities/toolsData.js
@@ -0,0 +1,82 @@
+const toolsData = [
+    {
+        name: "HTML",
+        iconifyName: "mdi:language-html5",
+        extraClass: "color-html",
+        href: "#"
+    },
+    {
+        name: "CSS",
+        iconifyName: "mdi:language-css3",
+        extraClass: "color-css",
+        href: "#"
+    },
+    {
+        name: "Bootstrap",
+        iconifyName: "mdi:bootstrap",
+        extraClass: "color-bootstrap",
+        href: "#"
+    },
+    {
+        name: "JavaScript",
+        iconifyName: "mdi:language-javascript",
+        extraClass: "color-js",
+        href: "#"
+    },
+    {
+        name: "React",
+        iconifyName: "mdi:react",
+        extraClass: "color-react",
+        href: "#"
+    },
+    {
+        name: "Astro",
+        iconifyName: "simple-icons:astro",
+        extraClass: "color-astro",
+        href: "#"
+    },
+    {
+        name: "Material UI",
+        iconifyName: "mdi:material-ui",
+        extraClass: "color-mui",
+        href: "#"
+    },
+    {
+        name: "Node JS",
+        iconifyName: "mdi:nodejs",
+        extraClass: "color-node",
+        href: "#"
+    },
+    {
+        name: "MySQL",
+        iconifyName: "cib:mysql",
+        extraClass: "color-mysql",
+        href: "#"
+    },
+    {
+        name: "Express",
+        iconifyName: "simple-icons:express",
+        extraClass: "color-express",
+        href: "#"
+    },
+    {
+        name: "MongoDB",
+        iconifyName: "simple-icons:mongodb",
+        extraClass: "color-mongodb",
+        href: "#"
+    },
+    {
+        name: "Git",
+        iconifyName: "mdi:git",
+        extraClass: "color-git",
+        href: "#"
+    },
+    {
+        name: "Github",
+        iconifyName: "mdi:github",
+        extraClass: "color-github",
+        href: "#"
+    },
+]
+
+export default toolsData
