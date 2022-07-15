a30f981ccd6590979e9e8f322cd21622b5a070a1
alvinAdler <alvinadlersampow@gmail.com>
2022-07-15 15:05:14 +0800

    Added actual links to the logos in the tools section

--- src/utilities/toolsData.js
+++ src/utilities/toolsData.js
@@ -3,79 +3,79 @@ const toolsData = [
         name: "HTML",
         iconifyName: "mdi:language-html5",
         extraClass: "color-html",
-        href: "#"
+        href: "https://www.w3schools.com/html/"
     },
     {
         name: "CSS",
         iconifyName: "mdi:language-css3",
         extraClass: "color-css",
-        href: "#"
+        href: "https://www.w3schools.com/css/"
     },
     {
         name: "Bootstrap",
         iconifyName: "mdi:bootstrap",
         extraClass: "color-bootstrap",
-        href: "#"
+        href: "https://getbootstrap.com/"
     },
     {
         name: "JavaScript",
         iconifyName: "mdi:language-javascript",
         extraClass: "color-js",
-        href: "#"
+        href: "https://www.javascript.com/"
     },
     {
         name: "React",
         iconifyName: "mdi:react",
         extraClass: "color-react",
-        href: "#"
+        href: "https://reactjs.org/"
     },
     {
         name: "Astro",
         iconifyName: "simple-icons:astro",
         extraClass: "color-astro",
-        href: "#"
+        href: "https://astro.build/"
     },
     {
         name: "Material UI",
         iconifyName: "mdi:material-ui",
         extraClass: "color-mui",
-        href: "#"
+        href: "https://mui.com/"
     },
     {
         name: "Node JS",
         iconifyName: "mdi:nodejs",
         extraClass: "color-node",
-        href: "#"
+        href: "https://nodejs.org/en/"
     },
     {
         name: "MySQL",
         iconifyName: "cib:mysql",
         extraClass: "color-mysql",
-        href: "#"
+        href: "https://www.mysql.com/"
     },
     {
         name: "Express",
         iconifyName: "simple-icons:express",
         extraClass: "color-express",
-        href: "#"
+        href: "https://expressjs.com/"
     },
     {
         name: "MongoDB",
         iconifyName: "simple-icons:mongodb",
         extraClass: "color-mongodb",
-        href: "#"
+        href: "https://www.mongodb.com/"
     },
     {
         name: "Git",
         iconifyName: "mdi:git",
         extraClass: "color-git",
-        href: "#"
+        href: "https://git-scm.com/"
     },
     {
         name: "Github",
         iconifyName: "mdi:github",
         extraClass: "color-github",
-        href: "#"
+        href: "https://github.com/"
     },
 ]
 
