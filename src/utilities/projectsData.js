68cbf6eb030247b12500ccc9de43f21de128f35a
alvinAdler <alvinadlersampow@gmail.com>
2022-08-08 19:55:56 +0800

    Removed live demo videos

--- src/utilities/projectsData.js
+++ src/utilities/projectsData.js
@@ -4,7 +4,7 @@ const projectsData = [
         projectTitle: "Game and Drill-based Touch Typing Education Application",
         projectDesc: "Have you ever heard of touch-typing? It is a typing method that emphasize the users to type with all of their fingers; covering as much parts of a keyboard with ease. This application offers two ways to learning touch-typing which are the drill mode and the game mode. For each session, the application will keep track of the performance of the user and display it in a nice visualization.",
         projectDetails: {
-            "youtube": "https://youtu.be/XFZ6kG53ZAA",
+            // "youtube": "https://youtu.be/XFZ6kG53ZAA",
             "github": "https://github.com/alvinAdler/TouchTypingApplication"
         }
     },
@@ -13,7 +13,7 @@ const projectsData = [
         projectTitle: "Simple Timeline Post",
         projectDesc: "This is a website that on most, a lite version of Twitter. This is a website that allows the users to share blogs among their friends. Write a headline, a couple of paragraphs, and press that \"share\" button and that's it! As simple as that. The users can also discover other users by typing their username in the search field. Send a request to that user and wait for approval. Once the other user accepts the request, both of them are can now see each other's posts.",
         projectDetails: {
-            "youtube": "https://youtu.be/JJPu7mrvzS4",
+            // "youtube": "https://youtu.be/JJPu7mrvzS4",
             "github": "https://github.com/alvinAdler/SimpleTimelinePost"
         }
     },
@@ -31,7 +31,7 @@ const projectsData = [
         projectTitle: "Laundry Invoice Management System",
         projectDesc: "Managing invoices of clothes in a laundry can get pretty messy and hard to maintain over years especially when the owner is doing it manually with pen and papers. With this application, the laundry owner can boost the time efficiency of invoice management. It can calculate the total price of the laundry clothes in real-time as the user input the information of the clothes, it can track the status / processing state of the mentioned clothes, and if the owner input the employees data into the application, the application can also calculate the salary of the employees! The owner just need to tell which employee process which clothes.",
         projectDetails: {
-            "youtube": "https://youtu.be/PhOp90-MWKM",
+            // "youtube": "https://youtu.be/PhOp90-MWKM",
             "github": null
         }
     },
